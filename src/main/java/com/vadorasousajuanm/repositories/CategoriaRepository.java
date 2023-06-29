6164427dec38f5fb26a1a3ac828c8f4756234ce5
Juan Sousa <juanmsousavadora@gmail.com>
2023-06-29 12:12:08 -0400

    Creando repository y service para Categoria

---
+++ src/main/java/com/vadorasousajuanm/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.vadorasousajuanm.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vadorasousajuanm.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
